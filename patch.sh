e66d7a2bcc0232d4a30bdbd6fbfd8bbb80496d2f
Adrian Castro <22133246+castdrian@users.noreply.github.com>
2025-01-24 16:49:53 +0100

    chore: vsc stuff and local build script

--- patch.sh
+++
@@ -1,8 +0,0 @@
-# Remove previous packages.
-rm -rf packages
-
-# Compile into .deb
-gmake clean package LOGS=1
-
-# Rename newly created .deb
-find packages/*.deb -exec sh -c 'mv "$0" packages/Unbound.deb' {} \;
