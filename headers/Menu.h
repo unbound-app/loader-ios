30bc612cd7cdf0139325bd4bb5e45f3383862b9a
Adrian Castro <22133246+castdrian@users.noreply.github.com>
2025-02-06 11:34:05 +0100

    chore: cleanup menu header

--- headers/Menu.h
+++ headers/Menu.h
@@ -10,23 +10,11 @@
 
 @interface UnboundMenuViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
 @property (nonatomic, strong) UITableView *tableView;
+- (void)dismiss;
 @end
 
 extern id gBridge;
-extern BOOL isJailbroken;
+
 BOOL isSafeModeEnabled(void);
 NSString *getDeviceIdentifier(void);
-void reloadApp(UIViewController *viewController);
-void deletePlugins(void);
-void deleteThemes(void);
-void deleteAllData(UIViewController *presenter);
-void refetchBundle(UIViewController *presenter);
-void toggleSafeMode(void);
-void setCustomBundleURL(NSURL *url, UIViewController *presenter);
-void resetCustomBundleURL(UIViewController *presenter);
-void showBundleSelector(UIViewController *presenter);
-void removeCachedBundle(void);
-void gracefulExit(UIViewController *presenter);
-void deletePluginsAndReload(UIViewController *presenter);
-void deleteThemesAndReload(UIViewController *presenter);
 void showMenuSheet(void);
