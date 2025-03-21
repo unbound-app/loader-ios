064db270d02789bf69b257d3712f9ecfaf466c5d
Adrian Castro <22133246+castdrian@users.noreply.github.com>
2025-03-21 12:00:28 +0100

    feat: adjust local build script for patcher changes and add fancy di sc view

--- headers/Utilities.h
+++ headers/Utilities.h
@@ -35,4 +35,8 @@
 + (BOOL)isAppStoreApp;
 + (BOOL)isJailbroken;
 
++ (NSString *)getDeviceModelIdentifier;
++ (BOOL)deviceHasDynamicIsland;
++ (void)addDynamicIslandOverlay;
+
 @end
--- sources/Utilities.m
+++ sources/Utilities.m
@@ -1,5 +1,7 @@
 #import <dlfcn.h>
+#include <sys/utsname.h>
 
+#import "DeviceModels.h"
 #import "Utilities.h"
 
 @implementation Utilities
@@ -210,6 +212,171 @@ static NSString *bundle = nil;
 + (BOOL)isJailbroken
 {
     return [[NSFileManager defaultManager] fileExistsAtPath:@"/var/jb"];
+}
+
++ (NSString *)getDeviceModelIdentifier
+{
+    struct utsname systemInfo;
+    uname(&systemInfo);
+    return [NSString stringWithCString:systemInfo.machine encoding:NSUTF8StringEncoding];
+}
+
++ (BOOL)deviceHasDynamicIsland
+{
+    NSString *identifier           = [self getDeviceModelIdentifier];
+    NSArray  *dynamicIslandDevices = @[
+        @"iPhone15,2", @"iPhone15,3", @"iPhone15,4", @"iPhone15,5", @"iPhone16,1", @"iPhone16,2",
+        @"iPhone17,1", @"iPhone17,2", @"iPhone17,3", @"iPhone17,4"
+    ];
+
+    return [dynamicIslandDevices containsObject:identifier];
+}
+
++ (UIImage *)createLogoImage
+{
+    CGFloat size = 512.0;
+    UIGraphicsBeginImageContextWithOptions(CGSizeMake(size, size), NO, 0);
+
+    [[UIColor whiteColor] setFill];
+
+    // Draw right part
+    UIBezierPath *rightPath = [UIBezierPath bezierPath];
+    [rightPath moveToPoint:CGPointMake(272.52, 177.27)];
+    [rightPath addLineToPoint:CGPointMake(277.81, 215.63)];
+    [rightPath addLineToPoint:CGPointMake(338.67, 215.74)];
+    [rightPath addLineToPoint:CGPointMake(338.67, 215.83)];
+    [rightPath addCurveToPoint:CGPointMake(373.01, 240.88)
+                 controlPoint1:CGPointMake(345.73, 216.18)
+                 controlPoint2:CGPointMake(359.97, 225.73)];
+    [rightPath addLineToPoint:CGPointMake(349.25, 240.88)];
+    [rightPath addCurveToPoint:CGPointMake(333.37, 260.06)
+                 controlPoint1:CGPointMake(345.04, 240.88)
+                 controlPoint2:CGPointMake(333.37, 249.47)];
+    [rightPath addCurveToPoint:CGPointMake(349.25, 279.24)
+                 controlPoint1:CGPointMake(333.37, 270.65)
+                 controlPoint2:CGPointMake(345.04, 279.24)];
+    [rightPath addLineToPoint:CGPointMake(376.41, 279.24)];
+    [rightPath addCurveToPoint:CGPointMake(338.67, 313.64)
+                 controlPoint1:CGPointMake(373.86, 288.78)
+                 controlPoint2:CGPointMake(357.41, 308.18)];
+    [rightPath addLineToPoint:CGPointMake(338.67, 313.75)];
+    [rightPath addLineToPoint:CGPointMake(297.66, 313.64)];
+    [rightPath addLineToPoint:CGPointMake(302.95, 351.9)];
+    [rightPath addLineToPoint:CGPointMake(338.67, 352.01)];
+    [rightPath addCurveToPoint:CGPointMake(416.94, 279.23)
+                 controlPoint1:CGPointMake(378, 352.01)
+                 controlPoint2:CGPointMake(410.64, 320.52)];
+    [rightPath addLineToPoint:CGPointMake(473.61, 279.14)];
+    [rightPath addLineToPoint:CGPointMake(489.48, 240.77)];
+    [rightPath addLineToPoint:CGPointMake(415.05, 240.88)];
+    [rightPath addCurveToPoint:CGPointMake(338.67, 177.38)
+                 controlPoint1:CGPointMake(405.63, 204.23)
+                 controlPoint2:CGPointMake(375, 177.38)];
+    [rightPath addLineToPoint:CGPointMake(272.52, 177.27)];
+    [rightPath closePath];
+
+    // Draw left part
+    UIBezierPath *leftPath = [UIBezierPath bezierPath];
+    [leftPath moveToPoint:CGPointMake(164.04, 160.07)];
+    [leftPath addCurveToPoint:CGPointMake(87.66, 223.57)
+                controlPoint1:CGPointMake(127.71, 160.07)
+                controlPoint2:CGPointMake(97.08, 186.92)];
+    [leftPath addLineToPoint:CGPointMake(41.01, 223.57)];
+    [leftPath addLineToPoint:CGPointMake(25.14, 261.94)];
+    [leftPath addLineToPoint:CGPointMake(85.77, 261.94)];
+    [leftPath addCurveToPoint:CGPointMake(164.04, 334.7)
+                controlPoint1:CGPointMake(92.07, 303.24)
+                controlPoint2:CGPointMake(124.7, 334.7)];
+    [leftPath addLineToPoint:CGPointMake(243.41, 334.7)];
+    [leftPath addLineToPoint:CGPointMake(238.12, 296.34)];
+    [leftPath addLineToPoint:CGPointMake(164.04, 296.34)];
+    [leftPath addLineToPoint:CGPointMake(164.04, 296.26)];
+    [leftPath addCurveToPoint:CGPointMake(126.3, 261.94)
+                controlPoint1:CGPointMake(145.3, 296.01)
+                controlPoint2:CGPointMake(128.85, 271.48)];
+    [leftPath addLineToPoint:CGPointMake(153.46, 261.94)];
+    [leftPath addCurveToPoint:CGPointMake(169.33, 242.76)
+                controlPoint1:CGPointMake(157.67, 261.94)
+                controlPoint2:CGPointMake(169.33, 253.35)];
+    [leftPath addCurveToPoint:CGPointMake(153.46, 223.57)
+                controlPoint1:CGPointMake(169.33, 232.17)
+                controlPoint2:CGPointMake(157.67, 223.57)];
+    [leftPath addLineToPoint:CGPointMake(129.7, 223.57)];
+    [leftPath addCurveToPoint:CGPointMake(164.04, 198.44)
+                controlPoint1:CGPointMake(133.15, 216.35)
+                controlPoint2:CGPointMake(147.27, 198.89)];
+    [leftPath addLineToPoint:CGPointMake(164.04, 198.44)];
+    [leftPath addLineToPoint:CGPointMake(219.6, 198.44)];
+    [leftPath addLineToPoint:CGPointMake(214.3, 160.07)];
+    [leftPath addLineToPoint:CGPointMake(164.04, 160.07)];
+    [leftPath closePath];
+
+    [rightPath fill];
+    [leftPath fill];
+
+    UIImage *result = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+
+    return result;
+}
+
++ (void)addDynamicIslandOverlay
+{
+    if (![self deviceHasDynamicIsland])
+    {
+        return;
+    }
+
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 1 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+        CGFloat width  = 126.0;
+        CGFloat height = 37.33;
+
+        CGFloat screenWidth = [UIScreen mainScreen].bounds.size.width;
+        CGFloat x           = (screenWidth - width) / 2;
+        CGFloat y           = 11.0;
+
+        UIView *islandView         = [[UIView alloc] initWithFrame:CGRectMake(x, y, width, height)];
+        islandView.backgroundColor = [UIColor blackColor];
+
+        UIBezierPath *path =
+            [UIBezierPath bezierPathWithRoundedRect:islandView.bounds
+                                  byRoundingCorners:UIRectCornerAllCorners
+                                        cornerRadii:CGSizeMake(height / 2, height / 2)];
+
+        CAShapeLayer *maskLayer = [CAShapeLayer layer];
+        maskLayer.path          = path.CGPath;
+        islandView.layer.mask   = maskLayer;
+
+        UIImage     *logoImage = [self createLogoImage];
+        UIImageView *logoView  = [[UIImageView alloc] init];
+        logoView.image         = logoImage;
+        logoView.contentMode   = UIViewContentModeScaleAspectFit;
+
+        CGFloat logoHeight  = height * 0.99;
+        CGFloat aspectRatio = logoImage.size.width / logoImage.size.height;
+        CGFloat logoWidth   = logoHeight * aspectRatio;
+        logoView.frame =
+            CGRectMake((width - logoWidth) / 2, (height - logoHeight) / 2, logoWidth, logoHeight);
+
+        [islandView addSubview:logoView];
+
+        UIWindow *keyWindow = nil;
+        for (UIScene *scene in [UIApplication sharedApplication].connectedScenes)
+        {
+            if (scene.activationState == UISceneActivationStateForegroundActive)
+            {
+                keyWindow = ((UIWindowScene *) scene).windows.firstObject;
+                break;
+            }
+        }
+
+        if (keyWindow)
+        {
+            islandView.alpha = 1.0;
+            [keyWindow addSubview:islandView];
+            [keyWindow bringSubviewToFront:islandView];
+        }
+    });
 }
 
 @end
