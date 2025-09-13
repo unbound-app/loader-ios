35590dba176e63b75cbe489fa48652398acf07b2
Adrian Castro <22133246+castdrian@users.noreply.github.com>
2025-09-13 17:16:03 +0200

    refactor: pip stuff

--- headers/NativeBridge+PluginAPI.h
+++ headers/NativeBridge+PluginAPI.h
@@ -5,22 +5,18 @@
 
 @interface NativeBridge (PluginAPI)
 
-// Notification methods
 + (NSString *)showNotification:(NSString *)title 
                           body:(NSString *)body 
                      timeDelay:(NSNumber *)timeDelay 
                   soundEnabled:(NSNumber *)soundEnabled
                     identifier:(NSString *)identifier;
-
-// Picture in Picture video methods
 + (NSString *)playPiPVideo:(NSString *)videoURL;
-
-// Utility methods
 + (UIViewController *)topViewController;
++ (void)cleanupPiPResources;
++ (void)tryStartPiPWithRetries:(NSInteger)remaining;
 
 @end
 
-// Delegate for PiP functionality
-@interface NativeBridgePluginAPIDelegate : NSObject <AVPlayerViewControllerDelegate>
+@interface NativeBridgePluginAPIDelegate : NSObject <AVPlayerViewControllerDelegate, AVPictureInPictureControllerDelegate>
 + (instancetype)sharedDelegate;
 @end
--- sources/NativeBridge+PluginAPI.m
+++ sources/NativeBridge+PluginAPI.m
@@ -77,17 +77,109 @@
     [Logger info:LOG_CATEGORY_NATIVEBRIDGE format:@"PiP did stop"];
 }
 
+- (void)pictureInPictureControllerWillStartPictureInPicture:
+    (AVPictureInPictureController *)pictureInPictureController
+{
+    [Logger info:LOG_CATEGORY_NATIVEBRIDGE format:@"PiP controller will start"];
+}
+
+- (void)pictureInPictureControllerDidStartPictureInPicture:
+    (AVPictureInPictureController *)pictureInPictureController
+{
+    [Logger info:LOG_CATEGORY_NATIVEBRIDGE format:@"PiP controller did start successfully"];
+}
+
+- (void)pictureInPictureController:(AVPictureInPictureController *)pictureInPictureController
+    failedToStartPictureInPictureWithError:(NSError *)error
+{
+    [Logger error:LOG_CATEGORY_NATIVEBRIDGE
+           format:@"PiP controller failed to start: %@", error.localizedDescription];
+}
+
+- (void)pictureInPictureController:(AVPictureInPictureController *)pictureInPictureController
+    restoreUserInterfaceForPictureInPictureStopWithCompletionHandler:
+        (void (^)(BOOL))completionHandler
+{
+    [Logger info:LOG_CATEGORY_NATIVEBRIDGE format:@"Restoring UI after PiP (controller)"];
+    if (completionHandler)
+        completionHandler(YES);
+}
+
+- (void)pictureInPictureControllerWillStopPictureInPicture:
+    (AVPictureInPictureController *)pictureInPictureController
+{
+    [Logger info:LOG_CATEGORY_NATIVEBRIDGE format:@"PiP controller will stop"];
+}
+
+- (void)pictureInPictureControllerDidStopPictureInPicture:
+    (AVPictureInPictureController *)pictureInPictureController
+{
+    [Logger info:LOG_CATEGORY_NATIVEBRIDGE
+          format:@"PiP controller did stop; cleaning up resources"];
+    [NativeBridge cleanupPiPResources];
+}
+
 @end
 
 @implementation NativeBridge (PluginAPI)
 
-static AVPlayerViewController *currentPlayerViewController = nil;
+static AVPlayerViewController       *currentPlayerViewController = nil;
+static AVPictureInPictureController *currentPiPController        = nil;
+static AVPlayer                     *currentPlayer               = nil;
+static AVPlayerLayer                *currentPlayerLayer          = nil;
+static UIView                       *currentHostView             = nil;
+
++ (void)tryStartPiPWithRetries:(NSInteger)remaining
+{
+    if (!currentPiPController)
+        return;
+    if (currentPiPController.pictureInPictureActive)
+        return;
+    if (currentPiPController.pictureInPicturePossible)
+    {
+        [currentPiPController startPictureInPicture];
+        return;
+    }
+    if (remaining <= 0)
+        return;
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t) (0.1 * NSEC_PER_SEC)),
+                   dispatch_get_main_queue(),
+                   ^{ [NativeBridge tryStartPiPWithRetries:remaining - 1]; });
+}
+
++ (void)cleanupPiPResources
+{
+    if (currentPiPController && currentPiPController.pictureInPictureActive)
+    {
+        [currentPiPController stopPictureInPicture];
+    }
+    currentPiPController.delegate = nil;
+    currentPiPController          = nil;
+
+    if (currentPlayerLayer.superlayer)
+    {
+        [currentPlayerLayer removeFromSuperlayer];
+    }
+    currentPlayerLayer = nil;
+    if (currentHostView.superview)
+    {
+        [currentHostView removeFromSuperview];
+    }
+    currentHostView = nil;
+
+    if (currentPlayer)
+    {
+        [currentPlayer pause];
+    }
+    currentPlayer = nil;
+}
 
 + (NSString *)playPiPVideo:(NSString *)videoURL
 {
     if (!videoURL || [videoURL length] == 0)
     {
-        [Logger error:LOG_CATEGORY_NATIVEBRIDGE format:@"Video URL is required for PiP video player"];
+        [Logger error:LOG_CATEGORY_NATIVEBRIDGE
+               format:@"Video URL is required for PiP video player"];
         return nil;
     }
 
@@ -105,6 +197,10 @@ static AVPlayerViewController *currentPlayerViewController = nil;
         {
             [currentPlayerViewController dismissViewControllerAnimated:NO completion:nil];
         }
+        if (currentPiPController && currentPiPController.pictureInPictureActive)
+        {
+            [currentPiPController stopPictureInPicture];
+        }
 
         NSURL *url = [NSURL URLWithString:videoURL];
         if (!url)
@@ -113,41 +209,93 @@ static AVPlayerViewController *currentPlayerViewController = nil;
             return;
         }
 
+        @try
+        {
+            NSError        *audioError = nil;
+            AVAudioSession *session    = [AVAudioSession sharedInstance];
+            BOOL ok = [session setCategory:AVAudioSessionCategoryPlayback
+                               withOptions:AVAudioSessionCategoryOptionDuckOthers
+                                     error:&audioError];
+            if (!ok || audioError)
+            {
+                [Logger error:LOG_CATEGORY_NATIVEBRIDGE
+                       format:@"Audio session category error: %@", audioError.localizedDescription];
+            }
+            audioError = nil;
+            ok         = [session setActive:YES error:&audioError];
+            if (!ok || audioError)
+            {
+                [Logger error:LOG_CATEGORY_NATIVEBRIDGE
+                       format:@"Audio session activate error: %@", audioError.localizedDescription];
+            }
+        }
+        @catch (NSException *exception)
+        {
+            [Logger error:LOG_CATEGORY_NATIVEBRIDGE
+                   format:@"Audio session exception: %@", exception.reason];
+        }
+
         AVPlayerItem *playerItem = [AVPlayerItem playerItemWithURL:url];
-        AVPlayer     *player     = [AVPlayer playerWithPlayerItem:playerItem];
+        currentPlayer            = [AVPlayer playerWithPlayerItem:playerItem];
+        currentPlayer.muted      = NO;
+        currentPlayer.volume     = 1.0;
 
-        currentPlayerViewController          = [[AVPlayerViewController alloc] init];
-        currentPlayerViewController.player   = player;
-        currentPlayerViewController.delegate = [NativeBridgePluginAPIDelegate sharedDelegate];
-
-        currentPlayerViewController.allowsPictureInPicturePlayback                  = YES;
-        currentPlayerViewController.canStartPictureInPictureAutomaticallyFromInline = YES;
+        currentPlayerLayer              = [AVPlayerLayer playerLayerWithPlayer:currentPlayer];
+        currentPlayerLayer.videoGravity = AVLayerVideoGravityResizeAspect;
 
         UIViewController *topViewController = [self topViewController];
-        if (topViewController)
+        if (!topViewController)
         {
-            [topViewController
-                presentViewController:currentPlayerViewController
-                             animated:YES
-                           completion:^{
-                               [Logger info:LOG_CATEGORY_NATIVEBRIDGE
-                                     format:@"PiP video player presented with ID: %@", playerId];
+            [Logger error:LOG_CATEGORY_NATIVEBRIDGE
+                   format:@"No top view controller found to host PiP layer"];
+            return;
+        }
 
-                               [player play];
+        if (!currentHostView)
+        {
+            currentHostView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 1, 1)];
+            currentHostView.userInteractionEnabled = NO;
+            currentHostView.alpha                  = 0.01;
+            [topViewController.view addSubview:currentHostView];
+        }
+        currentPlayerLayer.frame = currentHostView.bounds;
+        [currentHostView.layer addSublayer:currentPlayerLayer];
 
-                               dispatch_after(
-                                   dispatch_time(DISPATCH_TIME_NOW, (int64_t) (1.0 * NSEC_PER_SEC)),
-                                   dispatch_get_main_queue(), ^{
-                                       [Logger info:LOG_CATEGORY_NATIVEBRIDGE
-                                             format:@"PiP video player ready for user interaction"];
-                                   });
-                           }];
+        if (@available(iOS 15.0, *))
+        {
+            AVPictureInPictureControllerContentSource *source =
+                [[AVPictureInPictureControllerContentSource alloc]
+                    initWithPlayerLayer:currentPlayerLayer];
+            currentPiPController =
+                [[AVPictureInPictureController alloc] initWithContentSource:source];
         }
         else
         {
-            [Logger error:LOG_CATEGORY_NATIVEBRIDGE
-                   format:@"No top view controller found to present PiP player"];
+            currentPiPController =
+                [[AVPictureInPictureController alloc] initWithPlayerLayer:currentPlayerLayer];
         }
+        currentPiPController.delegate = [NativeBridgePluginAPIDelegate sharedDelegate];
+        currentPiPController.canStartPictureInPictureAutomaticallyFromInline = YES;
+
+        [Logger info:LOG_CATEGORY_NATIVEBRIDGE
+              format:@"Starting playback and requesting PiP for ID: %@", playerId];
+
+        [currentPlayer play];
+
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t) (0.1 * NSEC_PER_SEC)),
+                       dispatch_get_main_queue(), ^{
+                           if (currentPiPController &&
+                               currentPiPController.pictureInPicturePossible)
+                           {
+                               [currentPiPController startPictureInPicture];
+                           }
+                           else
+                           {
+                               [Logger info:LOG_CATEGORY_NATIVEBRIDGE
+                                     format:@"PiP not yet possible; retrying..."];
+                               [NativeBridge tryStartPiPWithRetries:20];
+                           }
+                       });
     });
 
     return playerId;
