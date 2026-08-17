e8d66b1dbc1c396bc21e8fcf4b9c109203f08659
Adrian Castro <22133246+castdrian@users.noreply.github.com>
2026-08-17 21:35:48 +0200

    refactor(security): co-locate attestation placeholder

--- sources/MachOAttestation.mm
+++ sources/MachOAttestation.mm
@@ -12,6 +12,11 @@
 
 #include <cstring>
 
+#if ATTESTATION_ENABLED
+__attribute__((section("__TEXT,__attestation"), used))
+const unsigned char AttestationPlaceholder[ATTESTATION_SECTION_SIZE] = {0};
+#endif
+
 struct AttestationSliceInfo
 {
     size_t        size;
